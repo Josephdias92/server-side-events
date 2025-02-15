e9fae3ed15b2116769eef2c5717f01435b794dbb
Joseph Dias <josephdias92@gmail.com>
2025-02-15 19:09:24 +0000

    add development script and implement fake authentication for SSE

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,12 +4,24 @@ import cors from "cors";
 const app = express();
 const port = 3000;
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 
 app.get("/", async (req: Request, res: Response) => {
+  // testing fake authentication
+  const username = req.headers.cookie?.split("=")[1];
+  if (username !== "Joey") {
+    res.status(401).send("Unauthorized");
+    return;
+  }
   res.setHeader("Content-Type", "text/event-stream");
   res.setHeader("Cache-Control", "no-cache");
   res.setHeader("Connection", "keep-alive");
+  res.flushHeaders();
   for (let i = 0; i < 10; i++) {
     res.write(`data: Hello, world! ${i}\n\n`);
     await new Promise((resolve) => setTimeout(resolve, 2000));
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,9 +2,15 @@ import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
+  // testing fake authentication
+  document.cookie =
+    "username=Joey; expires=Fri, 31 Dec 2025 23:59:59 GMT; path=/";
+
   const [data, setData] = useState<string[]>([]);
   useEffect(() => {
-    const eventSource = new EventSource("http://localhost:3000");
+    const eventSource = new EventSource("http://localhost:3000", {
+      withCredentials: true,
+    });
     eventSource.onmessage = (event) => {
       console.log(event);
       setData((prevData) => [...prevData, event.data]);
